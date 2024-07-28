9a33f3847a8590aadc373d245992dc2ae0716c0f
ryandeardorff <ryandeardorff@users.noreply.github.com>
2024-07-27 18:04:16 -0700

    Added :vs vertical split keybind '<leader>vs'

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -2,6 +2,7 @@ vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>')
 vim.keymap.set('n', '<leader>;', '<cmd>Alpha<CR>')
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
 vim.keymap.set('n', '<leader>q', '<cmd>:q!<CR>')
+vim.keymap.set('n', '<leader>vs', '<cmd>:vs<CR>')
 
 vim.keymap.set('n', 'g<S-c>', 'gcc', { remap = true })
 
