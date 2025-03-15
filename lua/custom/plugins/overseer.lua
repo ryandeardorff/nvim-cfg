653c5f02dd9ac5f8092278c214f05e8b9aa75a9d
Ryan Deardorff <ryansdeardorff@gmail.com>
2025-03-15 00:43:09 -0700

    resolved overseer keybind conflicts

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -1,31 +1,34 @@
-vim.api.nvim_create_user_command('Make', function(params)
-  -- Insert args at the '$*' in the makeprg
-  local cmd, num_subs = vim.o.makeprg:gsub('%$%*', params.args)
-  if num_subs == 0 then
-    cmd = cmd .. ' ' .. params.args
-  end
-  local task = require('overseer').new_task {
-    cmd = vim.fn.expandcmd(cmd),
-    components = {
-      { 'on_output_quickfix', open = false, set_diagnostics = true },
-      { 'qfnotify' },
-      'default',
-    },
-  }
-  task:start()
-end, {
-  desc = 'Run your makeprg as an Overseer task',
-  nargs = '*',
-})
-vim.keymap.set('n', '<leader>om', '<cmd>Make<CR>')
-vim.keymap.set('n', '<leader>oo', '<cmd>OverseerToggle<CR>')
-vim.keymap.set('n', '<leader>or', '<cmd>OverseerRun<CR>')
-
 return {
   {
     'stevearc/overseer.nvim',
     opts = {},
-    lazy = true,
-    keys = { '<leader>om', '<leader>oo', '<leader>or' },
+    -- lazy = true,
+    -- keys = { '<leader>om', '<leader>oo', '<leader>or' },
+    config = function()
+      local overseer = require 'overseer'
+      overseer.setup()
+      vim.api.nvim_create_user_command('Make', function(params)
+        -- Insert args at the '$*' in the makeprg
+        local cmd, num_subs = vim.o.makeprg:gsub('%$%*', params.args)
+        if num_subs == 0 then
+          cmd = cmd .. ' ' .. params.args
+        end
+        local task = require('overseer').new_task {
+          cmd = vim.fn.expandcmd(cmd),
+          components = {
+            { 'on_output_quickfix', open = false, set_diagnostics = true },
+            { 'qfnotify' },
+            'default',
+          },
+        }
+        task:start()
+      end, {
+        desc = 'Run your makeprg as an Overseer task',
+        nargs = '*',
+      })
+      vim.keymap.set('n', '<leader>om', '<cmd>Make<CR>')
+      vim.keymap.set('n', '<leader>oo', '<cmd>OverseerToggle<CR>')
+      vim.keymap.set('n', '<leader>or', '<cmd>OverseerRun<CR>')
+    end,
   },
 }
--- lua/custom/plugins/symbol_outline.lua
+++ lua/custom/plugins/symbol_outline.lua
@@ -4,7 +4,7 @@ return {
     lazy = true,
     cmd = { 'Outline', 'OutlineOpen' },
     keys = { -- Example mapping to toggle outline
-      { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+      { '<leader>to', '<cmd>Outline<CR>', desc = 'Toggle outline' },
     },
     opts = {
       preview_window = {},
